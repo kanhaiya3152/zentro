8f21c91a90156d3520d66f57c4a6bdcb3ef92d9b
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-04-10 22:46:14 +0530

    Add Bottom Navigation Bar with Home, Form, and Music Screens

---
+++ lib/bottom_navigation.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:zentro/audioplayer.dart';
+import 'package:zentro/form.dart';
+import 'package:zentro/home_screen.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int currentPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    // final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        backgroundColor:  Colors.white,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.grey[400],
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home_rounded),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.audio_file),
+            icon: Icon(Icons.audio_file_outlined),
+            label: 'Form',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.summarize_sharp),
+            icon: Icon(Icons.summarize_outlined),
+            label: 'Music',
+          ),
+        ],
+      ),
+      body: <Widget>[
+         HomeScreen(),
+         FormScreen(),
+         AudioScreen(),
+      ][currentPageIndex],
+    );
+  }
+}
