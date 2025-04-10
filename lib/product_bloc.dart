a9eb75efb348eec45e704e1b97c31cc7e5a5924b
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-04-11 01:51:06 +0530

    Add ProductBloc to manage product fetching and state management

---
+++ lib/product_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zentro/product_management.dart';
+import 'package:zentro/service.dart';
+import 'product_state.dart';
+
+class ProductBloc extends Bloc<ProductEvent, ProductState> {
+  final ProductRepository repository;
+  ProductBloc(this.repository) : super(ProductInitial()) {
+    on<FetchProducts>((event, emit) async {
+      emit(ProductLoading());
+      try {
+        final products = await repository.fetchProducts();
+        emit(ProductLoaded(products));
+      } catch (e) {
+        emit(ProductError(e.toString()));
+      }
+    });
+  }
+}
